4224a5d7099abf4b0849c216996af12ea4699dc6
neeraja1316 <neeraja.taduvayi@gmail.com>
2024-04-16 21:57:36 +0530

    first commit

---
+++ src/test/java/day1/Datatypes.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        int age = 30;
+        System.out.println("Age: "+age);
+        int x = 10;
+        int y = 10;
+        int sum = x+y;
+        System.out.println("sum: "+sum);
+
+        Short pin = 1234;
+        System.out.println("pin: "+pin);
+        Long aadhar =7362874683l;
+        System.out.println("aadhar: "+aadhar);
+        float weight = 66.56f;
+        System.out.println("weight: "+weight);
+        Double rate = 46746.87;
+        System.out.println("rate: "+rate);
+    }
+}
